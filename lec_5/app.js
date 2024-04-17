40c153ad13667cefde60729317c1122f00d1da0f
dipu kumar <dipukumardevcod@email.com>
2024-04-17 22:56:33 +0530

    learn about session and cookies

--- lec_5/app.js
+++ lec_5/app.js
@@ -2,11 +2,10 @@ const express = require('express')
 const app = express()
 
 
-app.use(function (req, res, next) {
-  console.log("dipu kumar");
-  next();
-})
-
+// use of the cookie
+// learn more two line in code 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.get('/', function (req, res) {
   res.send('Hello World')
---
+++ lec_5/cookie.md
@@ -0,0 +1,9 @@
+session,cookie
+
+when conected to the server to the based on cookies know as session;
+
+
+cookie matlab jo data aap ka browser par save hay wo data cookie hia ya ho fr par sav e hai wo data;
+
+
+this type of data us in authosication and login;
