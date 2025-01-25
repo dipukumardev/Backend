9752dd550944c4a5262e088976f797eb2415b21b
dipu kumar <dipukumardevcod@email.com>
2025-01-26 01:47:03 +0530

    Complet the 6 lecture of backend

--- lec_6/index.js
+++ lec_6/index.js
@@ -1,24 +1,39 @@
 const express = require('express');
+const path = require('path'); // Require path module for file paths
+
 const app = express();
-//  this is reqire to the the path ;
-const path = require('path');
 
+// Middleware for parsing JSON and URL-encoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Middleware to serve static files (CSS, JavaScript, images)
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use = (express.json());
-app.use = (express.urlencoded({ extended: true }));
+// Set EJS as the view engine
+app.set('view engine', 'ejs'); 
 
-// this line use to the css,venall js;
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.set('view engin', 'ejs');
-
-
-
+// Define the home route
 app.get("/", function (req, res) {
-    res.render("index.ejs");
-})
+    res.render("index"); // Automatically looks for "views/index.ejs"
+});
 
+
+app.get("/profile/:username", function(req,res){
+    res.send(`Welcome,  ${req.params.username}`);
+});
+
+app.get("/author/:username/:age",function(req, res){
+    // res.send(req.params);
+    res.send(`Welcome, ${req.params.username}  of age ${req.params.age}`)
+} )
+
+
+// Start the server
 app.listen(3000, function () {
-    console.log("good");
-})
+    console.log("Server is running on http://localhost:3000");
+});
 
+// Debugging helpers
+console.log("Current directory:", __dirname); 
+console.log("Public directory path:", path.join(__dirname, 'public')); 
