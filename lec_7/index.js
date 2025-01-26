ede626ade0c20fd6a597f2d3394077a530d060b3
dipu kumar <dipukumardevcod@email.com>
2025-01-26 10:49:35 +0530

    Complet lec 7

--- lec_7/index.js
+++ lec_7/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -10,7 +11,22 @@ app.use(express.static(path.join(__dirname,"public")));
 
 app.get('/', function(req, res)
 {
-   res.render("index");
+
+   // Read the directery of files;
+   fs.readdir(`./files`, function(err, files){
+      // console.log(files);
+      res.render("index", {files:files});
+   })
+});
+
+
+app.post('/create', function(req, res)
+{
+
+   fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,function(err)
+{
+   res.redirect("/");
+});
 });
 
 
